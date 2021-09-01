cc3ecba5b41769eff5b14b00efaa918df9a78356
Dhiraj Govindvira <dhirajgovindvira1112@gmail.com>
2021-09-01 18:26:36 +0530

    Lab1

--- Lab1/1.py
+++
@@ -1,4 +0,0 @@
-import nltk # Python library for NLP
-from nltk.corpus import twitter_samples # sample Twitter dataset from NLTK
-import matplotlib.pyplot as plt # library for visualization
-import random # pseudo-random number generator
